a113e68c93bd6514d9ab6b6af37f851bbfc22c43
merces1234 <vadinhomodesto9@gmail.com>
2025-08-25 22:00:15 -0300

    juntando com o servidor

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { GlobalStyles } from './styles/GlobalStyles.js';
-import Home from './App.jsx';
+import Home from './pages/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,6 @@
+import { useRef } from 'react'
+import api from '../../services/api'
+
 import { Container, 
   TopBackground, 
   Form, 
@@ -8,10 +11,23 @@ import { Container,
   Title 
 } from './styles';
 
-import UsersImage from './assets/users.png'
+import UsersImage from '../../assets/users.png'
 
 function Home() {
+  const inputName = useRef ()
+  const inputAge = useRef ()
+  const inputEmail = useRef ()
 
+    async function registerNewUser() {
+      const data = await api.post('/usuarios', {
+        email: inputEmail.current.value,
+        age: parseInt (inputAge.current.value),
+        name: inputName.current.value
+      })
+
+      console.log(data)
+    }
+    
   return (
     <Container>
       <TopBackground>
@@ -26,14 +42,14 @@ function Home() {
               <InputLabel>
                 Nome<span>*</span>
               </InputLabel>
-              <Input type='text' placeholder='Nome do Usuário' />
+              <Input type='text' placeholder='Nome do Usuário' ref={inputName}/>
             </div>
 
             <div>
               <InputLabel>
                 idade<span>*</span>
               </InputLabel>
-              <Input type='number' placeholder='Idade do Usuário' />
+              <Input type='number' placeholder='Idade do Usuário' ref={inputAge}/>
             </div>
           </ContainerInput>
 
@@ -41,10 +57,10 @@ function Home() {
             <InputLabel>
               idade<span>*</span>
             </InputLabel>
-            <Input type='email' placeholder='E-mail do Usuário' />
+            <Input type='email' placeholder='E-mail do Usuário' ref={inputEmail}/>
           </div>
 
-        <Button type='submit'>Cadantra Usuário</Button>
+        <Button type='button' onClick={registerNewUser}>Cadantra Usuário</Button>
 
       </Form>
     </Container>
